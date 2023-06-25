6d1dc6a3002f61355371901c2e7a2db76f1129c1
Igor <egor@sgf.lt>
2023-06-25 12:59:04 +0300

    Create meeting enums

---
+++ src/main/java/com/visma/meetings/model/MeetingCategory.java
@@ -0,0 +1,8 @@
+package com.visma.meetings.model;
+
+public enum MeetingCategory {
+    CODE_MONKEY,
+    HUB,
+    SHORT,
+    TEAM_BUILDING
+}
---
+++ src/main/java/com/visma/meetings/model/MeetingType.java
@@ -0,0 +1,6 @@
+package com.visma.meetings.model;
+
+public enum MeetingType {
+    LIVE,
+    IN_PERSON
+}
